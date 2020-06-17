9287f80beb20cd3ba076f360fb61ff31322620fc
shimada-code <nemigggy@gmail.com>
2020-06-18 04:00:56 +0900

    自動更新機能

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo__list">
             <div class="MessageInfo__list__userName">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo__list">
             <div class="MessageInfo__list__userName">
               ${message.user_name}
